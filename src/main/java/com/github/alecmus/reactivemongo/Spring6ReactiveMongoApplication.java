6c4f62fb0a1be849e2aed92b06b040e9a7402d19
Alec Musasa <alecmus@live.com>
2023-07-01 12:23:42 +0200

    Added Spring Initializr code

---
+++ src/main/java/com/github/alecmus/reactivemongo/Spring6ReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/alecmus/reactivemongo/Spring6ReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.reactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
