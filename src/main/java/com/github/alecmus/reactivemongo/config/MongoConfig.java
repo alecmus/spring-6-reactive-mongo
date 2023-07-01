10b0f5b96343c6c3091d27db853d8ea5ab217734
Alec Musasa <alecmus@live.com>
2023-07-01 17:38:12 +0200

    Added Mongo database authentication

--- src/main/java/com/github/alecmus/reactivemongo/config/MongoConfig.java
+++ src/main/java/com/github/alecmus/reactivemongo/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package com.github.alecmus.reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -18,4 +23,18 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     protected String getDatabaseName() {
         return "sfg";
     }
+
+    /*
+     * required when using Docker image
+     */
+//    @Override
+//    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+//        builder.credential(MongoCredential.createCredential("root",
+//                "admin", "example".toCharArray()))
+//                .applyToClusterSettings(settings -> {
+//                    settings.hosts(Collections.singletonList(
+//                            new ServerAddress("127.0.0.1, 27017")
+//                    ));
+//                });
+//    }
 }
