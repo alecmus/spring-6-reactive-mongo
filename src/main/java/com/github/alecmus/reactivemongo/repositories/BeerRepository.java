f997d93924d18fe699d58e047d76185d25bcd4da
Alec Musasa <alecmus@live.com>
2023-07-04 13:50:24 +0200

    Find by Beer style with Spring Data Mongo

--- src/main/java/com/github/alecmus/reactivemongo/repositories/BeerRepository.java
+++ src/main/java/com/github/alecmus/reactivemongo/repositories/BeerRepository.java
@@ -2,9 +2,12 @@ package com.github.alecmus.reactivemongo.repositories;
 
 import com.github.alecmus.reactivemongo.domain.Beer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface BeerRepository extends ReactiveMongoRepository<Beer, String> {
 
     Mono<Beer> findFirstByBeerName(String beerName);
+
+    Flux<Beer> findByBeerStyle(String beerStyle);
 }
--- src/main/java/com/github/alecmus/reactivemongo/services/BeerServiceImpl.java
+++ src/main/java/com/github/alecmus/reactivemongo/services/BeerServiceImpl.java
@@ -22,6 +22,12 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
+    public Flux<BeerDTO> findByBeerStyle(String beerStyle) {
+        return beerRepository.findByBeerStyle(beerStyle)
+                .map(beerMapper::beerToBeerDto);
+    }
+
+    @Override
     public Flux<BeerDTO> listBeers() {
         return beerRepository.findAll()
                 .map(beerMapper::beerToBeerDto);
