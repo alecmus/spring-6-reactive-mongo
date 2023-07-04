6f92d255658e6c72ef3faf99e0db6832e0d84476
Alec Musasa <alecmus@live.com>
2023-07-04 14:08:02 +0200

    Create BootstrapData.java

---
+++ src/main/java/com/github/alecmus/reactivemongo/bootstrap/BootstrapData.java
@@ -0,0 +1,67 @@
+package com.github.alecmus.reactivemongo.bootstrap;
+
+import com.github.alecmus.reactivemongo.domain.Beer;
+import com.github.alecmus.reactivemongo.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        beerRepository.deleteAll()
+                .doOnSuccess(success -> {
+                    loadBeerData();
+                })
+                .subscribe();
+    }
+
+    private void loadBeerData() {
+
+        beerRepository.count().subscribe(count -> {
+            if (count == 0) {
+                Beer beer1 = Beer.builder()
+                        .beerName("Galaxy Cat")
+                        .beerStyle("Pale Ale")
+                        .upc("12356")
+                        .price(new BigDecimal("12.99"))
+                        .quantityOnHand(122)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                Beer beer2 = Beer.builder()
+                        .beerName("Crank")
+                        .beerStyle("Pale Ale")
+                        .upc("12356222")
+                        .price(new BigDecimal("11.99"))
+                        .quantityOnHand(392)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                Beer beer3 = Beer.builder()
+                        .beerName("Sunshine City")
+                        .beerStyle("IPA")
+                        .upc("12356")
+                        .price(new BigDecimal("13.99"))
+                        .quantityOnHand(144)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                beerRepository.save(beer1).subscribe();
+                beerRepository.save(beer2).subscribe();
+                beerRepository.save(beer3).subscribe();
+            }
+        });
+    }
+}
